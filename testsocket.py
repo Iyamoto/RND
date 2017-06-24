453e30cd9c8a81f549c200b31a50ed904648c782
Iyamoto <Iyamoto@users.noreply.github.com>
2017-06-24 21:50:35 +0300

    testsocket upload

--- testsocket.py
+++ testsocket.py
@@ -1,21 +1,26 @@
 # Testing socket lib
 
 import socket
+import os
 import fire
 
 socket.setdefaulttimeout(2)
 
-def getbanner(ip, port):
+def getbanner(ip='127.0.0.1', port=21):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((ip, port))
         data = s.recv(1024)
-    return data
+    return data.decode('utf-8')
 
-data = getbanner('127.0.0.1', 21)
-print('Received', repr(data))
+# data = getbanner('127.0.0.1', 21)
+# print('Received', repr(data))
 
 if __name__ == '__main__':
-  fire.Fire(getbanner())
+    if os.environ.get('PYCHARM_HOSTED'):
+        data = getbanner()
+        print(data)
+    else:
+        fire.Fire(getbanner)
 
 # with open('README.md', 'r') as f:
 #     lines = f.readlines()
