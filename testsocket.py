8f5a850b426c49182ef79340c0e40cd112d3ebf4
Iyamoto <Iyamoto@users.noreply.github.com>
2017-06-24 23:57:17 +0300

    messing with socket

--- testsocket.py
+++ testsocket.py
@@ -3,8 +3,15 @@
 import socket
 import os
 import fire
+import nmap
 
 socket.setdefaulttimeout(2)
+
+def nmapScan(tgtHost, tgtPort):
+    nmScan = nmap.PortScanner()
+    nmScan.scan(tgtHost, tgtPort)
+    state=nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+    return state
 
 def getbanner(ip='127.0.0.1', port=21):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -16,8 +23,11 @@ if __name__ == '__main__':
     if os.environ.get('PYCHARM_HOSTED'):
         # data = getbanner()
         # print(data)
-        print(socket.gethostbyname('ya.ru'))
-        print(socket.gethostbyaddr('8.8.8.8'))
+        # print(socket.gethostbyname('ya.ru'))
+        # print(socket.gethostbyaddr('8.8.8.8'))
+        # scan = nmapScan('127.0.0.1', 21)
+        # print(scan)
+        exit(1)
     else:
         fire.Fire(getbanner)
 
