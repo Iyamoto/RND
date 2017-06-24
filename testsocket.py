54ea0a02e263d9fe959d1f7fb3664605340c25c5
Iyamoto <Iyamoto@users.noreply.github.com>
2017-06-24 21:38:31 +0300

    testsocket upload

--- testsocket.py
+++ testsocket.py
@@ -1,6 +1,7 @@
 # Testing socket lib
 
 import socket
+import fire
 
 socket.setdefaulttimeout(2)
 
@@ -12,3 +13,10 @@ def getbanner(ip, port):
 
 data = getbanner('127.0.0.1', 21)
 print('Received', repr(data))
+
+if __name__ == '__main__':
+  fire.Fire(getbanner())
+
+# with open('README.md', 'r') as f:
+#     lines = f.readlines()
+# print(lines)
