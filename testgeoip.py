c8ab9dd26b6593277ac79dd82996d603bf5d9d9c
Iyamoto <Iyamoto@users.noreply.github.com>
2017-06-25 17:07:57 +0300

    testing something

--- testgeoip.py
+++ testgeoip.py
@@ -4,13 +4,12 @@ import pygeoip
 import os
 import json
 
-geopath = os.path.join('data', 'GeoLiteCity.dat')
-gi = pygeoip.GeoIP(geopath)
-
 def getrecord(tgt):
+    geopath = os.path.join('data', 'GeoLiteCity.dat')
+    gi = pygeoip.GeoIP(geopath)
     rec = gi.record_by_name(tgt)
     return rec
 
-tgt = '173.255.226.98'
+tgt = '127.0.0.1'
 data = getrecord(tgt)
 print(json.dumps(data, indent=4))
