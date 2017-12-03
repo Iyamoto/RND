1915b178881af7f7957655240e55e6b7515161e3
iyamoto <iyamoto@gmail.com>
2017-12-03 15:26:04 +0300

    upload

--- griva.py
+++ griva.py
@@ -1,11 +1,19 @@
 """Letters permutation"""
 
 from itertools import permutations
+import os
 
 word = 'грива'
-perms = [''.join(p) for p in permutations(word)]
 
+# Get permutations
+perms = [''.join(p) for p in permutations(word)]
 print(len(set(perms)))
+
+# Load Russian wordlist
+with open(os.path.join('data', 'zaliznjak_forms.txt')) as word_file:
+    words = set(word.strip().lower() for word in word_file)
+
+print(words)
 
 for perm in sorted(set(perms)):
     print(perm)
