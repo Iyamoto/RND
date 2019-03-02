29e22f510b1feb4eac9da8c70ea6a71400539351
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-03-02 21:08:45 +0300

    refactoring

--- stocks/percent.py
+++ stocks/percent.py
@@ -20,13 +20,19 @@ income_rate = 0.05  # 0.03 - Conservative, 0.05 - Normal, 0.07 - Aggressive
 # Goals
 goal = dict()
 
-goal['fin_security'] = 10000  # For the flat
-goal['fin_security'] += 30000  # For food
-goal['fin_security'] += 10000  # For transportation
+goal['fin_security'] = 11000  # For the flat
+goal['fin_security'] += 40000  # For food
+goal['fin_security'] += 6000  # For transportation
+goal['fin_security'] += 2000  # For communications
 
-goal['style'] = goal['fin_security'] + 10000
+goal['style'] = goal['fin_security']
+goal['style'] += 7000  # Fitness club
+goal['style'] += 20000  # Dressings
+goal['style'] += 25000  # Travels
+goal['style'] += 10000  # Restaurants, theaters
+goal['style'] += 10000  # Beauty
 
-goal['freedom'] = goal['style'] + 100000
+goal['freedom'] = goal['style']
 
 achived = dict()
 achived['fin_security'] = 0
@@ -36,7 +42,7 @@ achived['freedom'] = 0
 print(json.dumps(goal, indent=4))
 
 income = start
-for i in range(30):
+for i in range(40):
     income += income * income_rate + add
     income_per_month = round(income * income_rate / 12)
 
