7d6fa93422a1d2dbd239108247a1ddae5ce8780f
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-03-03 11:24:43 +0300

    spiking

--- stocks/percent.py
+++ stocks/percent.py
@@ -5,10 +5,6 @@ from datetime import datetime
 import configs.settings
 
 # Add $ and rub
-# Add inflation
-
-# Initial state
-start = configs.settings.START_RUB
 
 # How much to invest?
 invest_rate = 0.25
@@ -16,54 +12,53 @@ add = configs.settings.MONTHLY_INCOME * 12 * invest_rate
 print('Yearly investment:', add)
 
 # Chose strategy
-income_rate = 0.08  # 0.05 - Conservative, 0.07 - Normal, 0.09 - Aggressive
+income_rate = 0.07  # 0.05 - Conservative, 0.07 - Normal, 0.09 - Aggressive
 inflation = 0.04
 
+# Prepare data
+data = dict()
+data['fin_security'] = dict()
+data['fin_security']['achived'] = 0
+data['style'] = dict()
+data['style']['achived'] = 0
+data['freedom'] = dict()
+data['freedom']['achived'] = 0
+
 # Goals
-goal = dict()
+data['fin_security']['goal'] = 11000  # For the flat
+data['fin_security']['goal'] += 40000  # For food
+data['fin_security']['goal'] += 6000  # For transportation
+data['fin_security']['goal'] += 2000  # For communications
 
-goal['fin_security'] = 11000  # For the flat
-goal['fin_security'] += 40000  # For food
-goal['fin_security'] += 6000  # For transportation
-goal['fin_security'] += 2000  # For communications
+data['style']['goal'] = data['fin_security']['goal']
+data['style']['goal'] += 7000  # Fitness club
+data['style']['goal'] += 20000  # Dressings
+data['style']['goal'] += 25000  # Travels
+data['style']['goal'] += 10000  # Restaurants, theaters
+data['style']['goal'] += 10000  # Beauty
 
-goal['style'] = goal['fin_security']
-goal['style'] += 7000  # Fitness club
-goal['style'] += 20000  # Dressings
-goal['style'] += 25000  # Travels
-goal['style'] += 10000  # Restaurants, theaters
-goal['style'] += 10000  # Beauty
-
-goal['freedom'] = goal['style']
-goal['freedom'] += 100000
-
-achived = dict()
-achived['fin_security'] = 0
-achived['style'] = 0
-achived['freedom'] = 0
-
-age = dict()
-
-print(json.dumps(goal, indent=4))
+data['freedom']['goal'] = data['style']['goal']
+data['freedom']['goal'] += 100000
 
 today = datetime.today()
 print('Now', today.year)
+print(json.dumps(data, indent=4))
 
-income = start
+income = configs.settings.START_RUB
 for i in range(1, 100):
     income += (income * income_rate + add) * (1 - inflation)
     income_per_month = round(income * income_rate / 12)
 
-    for key in goal:
-        if not achived[key]:
-            goal[key] = goal[key] * (1 + inflation)
-            if income_per_month >= goal[key]:
-                achived[key] = i
-                age[key] = today.year + i - configs.settings.BEARTH_YEAR
+    for key in data:
+        if not data[key]['achived']:
+            data[key]['goal'] = round(data[key]['goal'] * (1 + inflation), 0)
+            if income_per_month >= data[key]['goal']:
+                data[key]['achived'] = i
+                data[key]['age'] = today.year + i - configs.settings.BEARTH_YEAR
+                data[key]['income'] = round(income, 0)
 
-    if achived['fin_security'] and achived['style'] and achived['freedom']:
+    if data['fin_security']['achived'] and data['style']['achived'] and data['freedom']['achived']:
         break
 
-print(json.dumps(goal, indent=4))
-# print(json.dumps(achived, indent=4))
-print(json.dumps(age, indent=4))
+print('Future')
+print(json.dumps(data, indent=4))
